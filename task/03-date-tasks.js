9dba1ff6501e7f3942bbf01be1189da61b6901da
halaveika <halaveika1988@gmail.com>
2021-09-14 21:30:08 +0300

    feat: impliment 03-date-tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -1,4 +1,3 @@
-
 /** ******************************************************************************************
  *                                                                                          *
  * Plese read the following tutorial before implementing tasks:                             *
@@ -21,7 +20,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-  throw new Error('Not implemented');
+  return new Date(value);
 }
 
 /**
@@ -36,7 +35,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-  throw new Error('Not implemented');
+  return new Date(value);
 }
 
 
@@ -55,7 +54,10 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-  throw new Error('Not implemented');
+  if (date.getFullYear() % 4 !== 0) { return false;}
+  else if (date.getFullYear() % 100 !== 0) { return true;}
+  else if (date.getFullYear() % 400 !== 0) {return false;}
+  return true;
 }
 
 
@@ -75,12 +77,12 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-  throw new Error('Not implemented');
+  const tipmespan = Math.abs(startDate.getTime() - endDate.getTime());
+  return new Date(tipmespan).toISOString().slice(11, -1);
 }
 
-
 /**
- * Returns the angle (in radians) between the hands of an analog clock for the 
+ * Returns the angle (in radians) between the hands of an analog clock for the
  * specified Greenwich time.
  * If you have problem with solution please read: https://en.wikipedia.org/wiki/Clock_angle_problem
  *
@@ -94,7 +96,11 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-  throw new Error('Not implemented');
+  const data = new Date(date);
+  const HH = data.getUTCHours() < 12 ? data.getUTCHours() : data.getUTCHours() - 12;
+  const mm = data.getUTCMinutes();
+  const anlgleD = Math.abs((60 *HH - 11 * mm) * 0.5);
+  return (anlgleD > 180) ? ( 360 - anlgleD )  *  ( Math.PI / 180 ) : anlgleD * (Math.PI / 180);
 }
 
 module.exports = {
